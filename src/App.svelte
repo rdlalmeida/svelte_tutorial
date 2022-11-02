a7f59ed85f1667ced04bd6a1c921afa94d40d1a0
Ricardo Almeida <rdlalmeida@gmail.com>
2022-11-02 21:45:13 +0000

    latest changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,27 @@
 <script>
-	export let name;
+	let firstName = "Ricardo";
+	let lastName = "Almeida";
+	let color = 'yellow';
+	let showText = false;
+
+	$: name = firstName + ' ' + lastName;
+
+	const toggle = () => {
+		color = (color === 'blue' ? 'red' : 'blue');
+		showText = !showText;
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1 style="color: {color}">Hello {name}!</h1>
+	{#if showText}
+		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	
+	{:else}
+	<p>No Text</p>
+	{/if}
+
+	<button on:click={toggle}>Click me!</button>
 </main>
 
 <style>
