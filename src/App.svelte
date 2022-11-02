291f9586ded3b26efc5dee9e70b7e3ca33d60104
Ricardo Almeida <rdlalmeida@gmail.com>
2022-11-02 22:58:46 +0000

    Update

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,28 @@
 <script>
-	let firstName = "Ricardo";
-	let lastName = "Almeida";
-	let color = 'yellow';
-	let showText = false;
-
-	$: name = firstName + ' ' + lastName;
-
-	const toggle = () => {
-		color = (color === 'blue' ? 'red' : 'blue');
-		showText = !showText;
-	}
+	let feedback = [
+		{
+			id: 1,
+			rating: 10,
+			text: 'lorem ipsum dolor sit amet consecterur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+		},
+		{
+			id: 2,
+			rating: 9,
+			text: 'lorem ipsum dolor sit amet consecterur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+		},
+		{
+			id: 3,
+			rating: 8,
+			text: 'lorem ipsum dolor sit amet consecterur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+		}
+	]
 </script>
 
 <main>
-	<h1 style="color: {color}">Hello {name}!</h1>
-	{#if showText}
-		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello!</h1>
 	
-	{:else}
-	<p>No Text</p>
-	{/if}
-
-	<button on:click={toggle}>Click me!</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
